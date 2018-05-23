41accd37b6db4383b3b282586282a759f94e4e22
ArvinLleva <arvin.lleva@yahoo.com>
2018-05-23 05:39:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromEndOfArray(array){
   array.shift()
   return array
 }
