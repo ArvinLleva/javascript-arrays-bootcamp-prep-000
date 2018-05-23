99ff1692b5c2c02fbdefb9bde07f993d5f823928
ArvinLleva <arvin.lleva@yahoo.com>
2018-05-23 05:00:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-return [element,...array] // the ... is the spread operator
+return [element,...array] // the ... is the spread operator which spreads out the arrays contents
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element)
 }
 
-function addElementToEndOfArray(){
-  
+function addElementToEndOfArray(array, element){
+  return array.shift(element)
 }
 
 function destructivelyAddElementToEndOfArray(){
-  
+  return [...array, element]
 }
