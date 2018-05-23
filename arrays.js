b0b5ae6d4802fc73d24eff8f238924c825194d0f
ArvinLleva <arvin.lleva@yahoo.com>
2018-05-23 04:25:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,18 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBEginningOfArray(array, element){
+  
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  
+}
+
+function addElementToEndOfArray(){
+  
+}
+
+function destructivelyAddElementToEndOfArray(){
+  
+}
